4f7689d7ffd2c76cc4edfcbfeaea31119302241f
TKaburu <traceymwendwa@gmail.com>
2024-05-05 08:56:31 +0300

    Feat: Login functionality

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/home')
 def home():
     user = {'username': 'Tracey'}
     return render_template ('home.html', user=user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login Requested for user{}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('home'))
+    return render_template('login.html', form=form)
